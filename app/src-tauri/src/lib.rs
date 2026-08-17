72b64e873c54c033cfd086bb66d9a9b10d348e81
John Villafania <johnarnoldvillafania@gmail.com>
2026-08-17 23:25:55 +0800

    feat: tray app skeleton with keep-alive and single instance

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,14 +1,43 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+    RunEvent,
+};
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .setup(|app| {
+            let region = MenuItem::with_id(
+                app, "capture_region", "Capture Region\tCtrl+Shift+Space", true, None::<&str>,
+            )?;
+            let screen = MenuItem::with_id(app, "capture_screen", "Capture Screen", true, None::<&str>)?;
+            let quit = MenuItem::with_id(app, "quit", "Exit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&region, &screen, &quit])?;
+            TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .tooltip("Developer Visual Companion")
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id().as_ref() {
+                    "quit" => app.exit(0),
+                    "capture_region" => { /* Task 8 */ }
+                    "capture_screen" => { /* Task 9 */ }
+                    _ => {}
+                })
+                .build(app)?;
+            Ok(())
+        })
+        .build(tauri::generate_context!())
+        .expect("error building tauri app")
+        .run(|_app, event| {
+            // no persistent windows: keep the process alive when overlay/composer close,
+            // but let app.exit(0) (code is Some) actually quit
+            if let RunEvent::ExitRequested { api, code, .. } = event {
+                if code.is_none() {
+                    api.prevent_exit();
+                }
+            }
+        });
 }
